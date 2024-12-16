8b6d928732820389e87467df2be43fd53a3ea0bb
matija-sokol <matijasokol123@gmail.com>
2024-12-16 21:16:39 +0100

    chore: Change android ktor client

--- app/src/main/java/com/matijasokol/githubapp/di/NetworkModule.kt
+++ app/src/main/java/com/matijasokol/githubapp/di/NetworkModule.kt
@@ -6,7 +6,7 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import io.ktor.client.HttpClient
-import io.ktor.client.engine.android.Android
+import io.ktor.client.engine.okhttp.OkHttp
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.logging.Logging
 import io.ktor.client.plugins.logging.Logger
@@ -22,7 +22,7 @@ object NetworkModule {
     @Provides
     @Singleton
     fun provideHttpClient(): HttpClient {
-        return HttpClient(Android) {
+        return HttpClient(OkHttp) {
             install(Logging) {
                 logger = object : Logger {
                     override fun log(message: String) {
