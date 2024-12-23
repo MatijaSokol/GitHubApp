146d3ec9711169702edd5327d26a79ba6b8e4a45
matija-sokol <matijasokol123@gmail.com>
2024-12-23 09:59:15 +0100

    chore: Update details screen state logic

--- repo/detail/src/main/java/com/matijasokol/repo/detail/RepoDetailViewModel.kt
+++ repo/detail/src/main/java/com/matijasokol/repo/detail/RepoDetailViewModel.kt
@@ -4,18 +4,13 @@ import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.navigation.toRoute
-import arrow.core.Either
-import com.matijasokol.core.error.NetworkError
-import com.matijasokol.coreui.dictionary.Dictionary
 import com.matijasokol.coreui.navigation.Destination
-import com.matijasokol.repo.domain.model.Repo
 import com.matijasokol.repo.domain.usecase.GetRepoDetailsUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.combine
-import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.onStart
@@ -28,39 +23,26 @@ import javax.inject.Inject
 class RepoDetailViewModel @Inject constructor(
     savedStateHandle: SavedStateHandle,
     private val getRepoDetails: GetRepoDetailsUseCase,
-    private val dictionary: Dictionary,
+    private val uiMapper: RepoDetailsUiMapper,
 ) : ViewModel() {
 
     private val fetchTrigger = Channel<Unit>()
-
     private val isLoading = MutableStateFlow(true)
+
     private val repo = fetchTrigger.receiveAsFlow()
         .onStart { emit(Unit) }
         .map { savedStateHandle.toRoute<Destination.RepoDetail>().repoName }
         .onEach { isLoading.update { true } }
         .map(getRepoDetails::invoke)
         .onEach { isLoading.update { false } }
-        .stateIn(
-            scope = viewModelScope,
-            started = SharingStarted.WhileSubscribed(5_000L),
-            initialValue = null,
-        )
 
     val state = combine(
         isLoading,
-        repo.filterNotNull(),
-        ::toUiState,
+        repo,
+        uiMapper::toUiState,
     ).stateIn(
         scope = viewModelScope,
         started = SharingStarted.WhileSubscribed(5_000L),
-        initialValue = RepoDetailState(isLoading = true),
+        initialValue = RepoDetailState.Loading,
     )
-
-    private fun toUiState(
-        isLoading: Boolean,
-        repoOrError: Either<NetworkError, Repo>,
-    ) = when (repoOrError) {
-        is Either.Left -> RepoDetailState(isLoading = isLoading, errorMessage = "error")
-        is Either.Right -> RepoDetailState(isLoading = isLoading, repo = repoOrError.value)
-    }
 }
---
+++ repo/detail/src/main/java/com/matijasokol/repo/detail/RepoDetailsUiMapper.kt
@@ -0,0 +1,25 @@
+package com.matijasokol.repo.detail
+
+import arrow.core.Either
+import com.matijasokol.core.error.NetworkError
+import com.matijasokol.coreui.dictionary.Dictionary
+import com.matijasokol.repo.domain.model.Repo
+import javax.inject.Inject
+
+class RepoDetailsUiMapper @Inject constructor(
+    private val dictionary: Dictionary,
+) {
+
+    fun toUiState(
+        isLoading: Boolean,
+        repoOrError: Either<NetworkError, Repo>,
+    ) = when (isLoading) {
+        true -> RepoDetailState.Loading
+        false -> when (repoOrError) {
+            is Either.Left -> RepoDetailState.Error(
+                dictionary.getString(R.string.repo_detail_message_cache_error),
+            )
+            is Either.Right -> RepoDetailState.Success(repoOrError.value)
+        }
+    }
+}
