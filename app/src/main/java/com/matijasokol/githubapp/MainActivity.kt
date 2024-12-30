d76100169517b571276d527498363713e449be95
matija-sokol <matijasokol123@gmail.com>
2024-12-30 13:14:37 +0100

    feature: Add splash screen

--- app/src/main/java/com/matijasokol/githubapp/MainActivity.kt
+++ app/src/main/java/com/matijasokol/githubapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.matijasokol.githubapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.matijasokol.githubapp.navigation.Navigator
 import com.matijasokol.githubapp.ui.AppContent
 import com.matijasokol.githubapp.ui.theme.GitHubAppTheme
@@ -19,6 +20,8 @@ class MainActivity : ComponentActivity() {
     lateinit var modeChecker: ModeChecker
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
+
         super.onCreate(savedInstanceState)
 
         setContent {
