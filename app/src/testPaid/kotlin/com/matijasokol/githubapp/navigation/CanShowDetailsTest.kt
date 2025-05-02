fa3cacf8a7486d04cfb2414032400fe95705b94b
matija-sokol <matijasokol123@gmail.com>
2025-05-02 10:49:05 +0200

    chore: Update test after changing navigation

---
+++ app/src/testPaid/kotlin/com/matijasokol/githubapp/navigation/CanShowDetailsTest.kt
@@ -0,0 +1,21 @@
+package com.matijasokol.githubapp.navigation
+
+import com.matijasokol.githubapp.ModeChecker
+import org.amshove.kluent.`should be`
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class CanShowDetailsTest {
+
+    private lateinit var sut: CanShowDetailsUseCase
+
+    @BeforeEach
+    fun setUp() {
+        sut = CanShowDetailsUseCase(ModeChecker())
+    }
+
+    @Test
+    fun `should RETURN TRUE when use case is called`() {
+        sut() `should be` true
+    }
+}
