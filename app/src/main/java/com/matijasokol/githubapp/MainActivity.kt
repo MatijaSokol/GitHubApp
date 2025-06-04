0920673c2f30c149ebd6636ae271c67c383d7df6
matija-sokol <matijasokol123@gmail.com>
2025-06-04 17:50:10 +0200

    Enable edge to edge and update navigation transition to work without BoxWithConstraints

--- app/src/main/java/com/matijasokol/githubapp/MainActivity.kt
+++ app/src/main/java/com/matijasokol/githubapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.matijasokol.githubapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.matijasokol.githubapp.navigation.NavigationErrorMapper
 import com.matijasokol.githubapp.navigation.Navigator
@@ -22,6 +23,7 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
+        enableEdgeToEdge()
 
         super.onCreate(savedInstanceState)
 
