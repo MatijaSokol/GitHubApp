f9d943a6f9a627f4a3a8f8097a3cb99fc4eb0f55
matija-sokol <matijasokol123@gmail.com>
2025-06-25 18:16:50 +0200

    Migrate from Context Receivers to Context Parameters

--- core-ui/src/main/java/com/matijasokol/coreui/viewmodel/ViewModelExt.kt
+++ core-ui/src/main/java/com/matijasokol/coreui/viewmodel/ViewModelExt.kt
@@ -6,15 +6,15 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.stateIn
 
-context(ViewModel)
+context(viewModel: ViewModel)
 fun <T> Flow<T>.stateIn(
     initialValue: T,
     started: SharingStarted = SharingStarted.WhileSubscribed(STOP_TIMEOUT_MILLIS),
 ) = stateIn(
-    scope = viewModelScope,
+    scope = viewModel.viewModelScope,
     started = started,
     initialValue = initialValue,
 )
 
-context(ViewModel)
+context(_: ViewModel)
 val STOP_TIMEOUT_MILLIS get() = 5_000L
